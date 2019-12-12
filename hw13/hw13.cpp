d7ff3e3bea15cff04e5a9673e2d71f2065b5a264
Yizhou Gu <1842191969@qq.com>
2019-12-12 18:14:43 -0500

    yg2707

--- hw13/hw13.cpp
+++ hw13/hw13.cpp
@@ -29,3 +29,34 @@ AddIn xai_xllproject(
 );
 
 //!!! Implement XLL.PWLINEAR.VALUE as described above.
+AddIn xai_pwlinear_value(
+	Function(XLL_DOUBLE, L"?xll_pwlinear_value", CATEGORY L".PWLINEAR.VALUE")
+	.Arg(XLL_FP, L"x", L"is a sequence")
+	.Arg(XLL_FP, L"y", L"is a sequence")
+	.Arg(XLL_DOUBLE, L"f", L"is value of a")
+	.Arg(XLL_FP, L"p", L"is a sequence")
+	.Arg(XLL_FP, L"c", L"is a sequence")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return a two column array of cash flow time and amount.")
+	.Documentation(
+		L"The first column is the cash flow time and the second is the corresponding amount. "
+	)
+);
+
+
+double WINAPI xll_pwlinear_value(_FP12* x, _FP12* y, double f, _FP12* p, _FP12* c)
+{
+#pragma XLLEXPORT
+	size_t n = sizeof(x);
+	double* x_ = &x->array[0];
+	double* y_ = &y->array[0];
+	double* p_ = &p->array[0];
+	double* c_ = &c->array[0];
+	double* fnew = new double[n];
+
+	size_t i = fms::pwlinear_coefficients(f, n, x_, y_, fnew);
+	
+	double E = fms::pwlinear_value(n, fnew, i, p_, c_);
+	return E;
+
+}
