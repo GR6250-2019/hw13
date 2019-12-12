0ed015120ef37b762d079ccaf9e430dc7dbe237e
william <junho.song@columbia.edu>
2019-12-12 00:43:52 -0500

    js5178 William Song hw13

--- hw13/hw13.cpp
+++ hw13/hw13.cpp
@@ -1,6 +1,7 @@
 // xllproject.cpp
 #include <cmath>
 #include "hw13.h"
+#include "pwlinear.h"
 
 using namespace xll;
 
@@ -13,8 +14,8 @@ AddIn xai_project(
 );
 
 AddIn xai_xllproject(
-    Document(CATEGORY)
-    .Category(CATEGORY)
+	Document(CATEGORY)
+	.Category(CATEGORY)
     .Documentation(
         L"Documentation for the " CATEGORY L" category. "
         L"<para>"
@@ -28,4 +29,42 @@ AddIn xai_xllproject(
     )
 );
 
-//!!! Implement XLL.PWLINEAR.VALUE as described above.
+AddIn xai_pwlinear_value(
+	Function(XLL_DOUBLE, L"?xll_pwlinear_value", L"XLL.PWLINEAR.VALUE")
+	.Arg(XLL_FP, L"x", L"is an array of the x values of a piecewise linear payoff. ")
+	.Arg(XLL_FP, L"y", L"is an array of the y values of a piecewise linear payoff. ")
+	.Arg(XLL_DOUBLE, L"f", L"is the par forward value of the underlying. ")
+	.Arg(XLL_FP, L"p", L"is an array of puts for x < f. ")
+	.Arg(XLL_FP, L"c", L"is an array of calls for x > f. ")
+	.Volatile()
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the expected value of the payoff. ")
+);
+
+double WINAPI xll_pwlinear_value(_FP12* px, _FP12* py, double a, _FP12* pp, _FP12* pc) {
+#pragma XLLEXPORT
+
+	try {
+		auto n = size(*px);
+		ensure(n == size(*py));
+
+		double* x = &px->array[0];
+		double* y = &py->array[0];
+		double* p = &pp->array[0];
+		double* c = &pc->array[0];
+
+		static xll::FP12 result;
+		result.resize(n, 1);
+
+		std::vector<double> fv;
+		fv.resize(n);
+		double* f = fv.data();
+
+		size_t i = fms::pwlinear_coefficients(a, n, x, y, f);
+		return fms::pwlinear_value(n, f, i, p, c);
+		return i;
+	} catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+		return 0;
+	}
+}
--- hw13/pwlinear.h
+++ hw13/pwlinear.h
@@ -1,6 +1,9 @@
 // pwlinear.h - Fit a piecewise linear curve.
 #pragma once
-
+#include <Eigen/Core>
+#include <Eigen/LU>
+#include <iostream>
+using namespace std;
 namespace fms {
 
 	// Given a p.w. linear curve find the Carr-Madan coefficients.
@@ -12,10 +15,43 @@ namespace fms {
 	//        + f'(a)(x - a).
 	// Return i such that x[i] < a < x[i+1]
 	template<class X = double>
-	inline size_t pwlinear_coefficients(const X& a, size_t n, const X* x, const X* y, X* f)
+	inline size_t pwlinear_coefficients(const X & a, size_t n, const X * x, const X * y, X * f)
 	{
-		//!!! implement
-		return 0;
+		// Solve m*f = v for f
+		Eigen::MatrixXf m(n, n);
+		Eigen::VectorXf v(n);
+
+		size_t a_ind = 0;
+
+		// Initialize v with values from y
+		v(0) = y[0];
+		for (size_t ind = 1; ind < n; ind++) {
+			v(ind) = y[ind];
+			if (x[ind - 1] < a && x[ind] > a) {
+				a_ind = ind - 1;
+			}
+		}
+
+		for (size_t row = 0; row < n; row++) {
+			m(row, 0) = 1.0;
+			for (size_t col = 1; col < n - 1; col++) {
+				if (x[col] < a) {
+					m(row, col) = std::fmax(x[col] - x[row], 0.0);
+				}
+				else {
+					m(row, col) = std::fmax(x[row] - x[col], 0.0);
+				}
+				m(row, n - 1) = x[row] - a;
+			}
+		}
+
+		Eigen::MatrixXf m_inv = m.inverse();
+		Eigen::VectorXf v_prod = m_inv * v;
+		for (size_t ind = 0; ind < n; ind++) {
+			f[ind] = v_prod(ind);
+		}
+
+		return a_ind;
 	}
 
 	// Expected value of payoff.
@@ -27,6 +63,22 @@ namespace fms {
 	template<class X = double>
 	inline X pwlinear_value(size_t n, const X* f, size_t i, const X* p, const X* c)
 	{
-		return 0; //!!! implement
+		X E_f = f[0];
+		cout << "E_f " << f[0] << endl;
+		cout << "E_f " << E_f << endl;
+		// Sum the puts
+		for (int j = 1; j <= i; j++) {
+			E_f += f[j] * p[j];
+			cout << "E_p " << (f[j] * p[j - 1]) << endl;
+			cout << "E_p " << E_f << endl;
+		}
+		// Sum the calls
+		for (int j = i + 1; j < n - 1; j++) {
+			E_f += f[j] * c[j - i - 1];
+			cout << "E_c " << (f[j] * c[j-i-1]) << endl;
+			cout << "E_c " << E_f << endl;
+		}
+
+		return E_f;
 	}
 }
