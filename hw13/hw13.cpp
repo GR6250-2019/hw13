6991b669cc597e217929396018dad6767f574770
miaow1 <mw3302@columbia.edu>
2019-12-12 18:13:16 -0500

    hw13

--- hw13/hw13.cpp
+++ hw13/hw13.cpp
@@ -1,16 +1,34 @@
 // xllproject.cpp
- #include <cmath>
- #include "hw13.h"
- #include "pwlinear.h"
+#include <cmath>
+#include "hw13.h"
+#include "pwlinear.h"
+using namespace xll;
 
- using namespace xll;
+AddIn xai_project(
+    Document(L"hw13")
+    .Documentation(
+        L"This will generate a Sandcastle Helpfile Builder project file. "
+        L"Start Excel with the add-in loaded and run (Alt-F8) the macro MAKE.SHFB. "
+    )
+);
 
- @@ -28,4 +29,35 @@ AddIn xai_xllproject(
-     )
- );
+AddIn xai_xllproject(
+    Document(CATEGORY)
+    .Category(CATEGORY)
+    .Documentation(
+        L"Documentation for the " CATEGORY L" category. "
+        L"<para>"
+        L"The function XLL.PWLINEAR.VALUE(x, y, f, p, c) computes the value "
+        L"of a piecwise linear payoff determinded by x and y "
+        L"using put prices (x < f) and call prices (x > f). "
+        L"</para>"
+        L"<para>"
+        L"We assume f is par forward value of the underlying. "
+        L"</para>"
+    )
+);
 
- //!!! Implement XLL.PWLINEAR.VALUE as described above. 
- //!!! Implement XLL.PWLINEAR.VALUE as described above.
+//!!! Implement XLL.PWLINEAR.VALUE as described above.
  AddIn xai_pwlinear_value(
  	Function(XLL_DOUBLE, L"?xll_pwlinear_value", L"XLL.PWLINEAR.VALUE")
  	.Arg(XLL_FP, L"x",L"is an array of x-axis points.")
