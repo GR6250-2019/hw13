2a41f56e2d8fa340a0acaa75a3f2d24dd521c9d8
miaow1 <mw3302@columbia.edu>
2019-12-12 18:15:07 -0500

    hw13

--- hw13/pwlinear.h
+++ hw13/pwlinear.h
@@ -15,7 +15,36 @@ namespace fms {
 	inline size_t pwlinear_coefficients(const X& a, size_t n, const X* x, const X* y, X* f)
 	{
 		//!!! implement
-		return 0;
+		size_t res = 0;
+ 		for (size_t i = 0; i < n-1; i++) {
+ 			if (x[i]<a && x[i + 1]>a) {
+ 				res = i;
+ 				break;
+ 			}
+ 		}
+
+ 		std::vector<X> temp(n);
+ 		temp[n - 1] = (y[res + 1] - y[res]) / (x[res + 1] - x[res]);
+ 		temp[0] = y[res] + temp[n - 1] * (a - x[res]);
+
+ 		for (size_t k = res; k > 0; k--) {
+ 			X ans = y[k - 1] - y[k] -temp[n - 1] * (x[k - 1] - x[k]);
+ 			for (size_t j = res; j > k; j--) {
+ 				ans -= temp[j] * (x[k] - x[k - 1]);
+ 			}
+ 			temp[k] = ans / (x[k] - x[k - 1]);
+ 		}
+
+ 		for (size_t k = res + 1; k <= n - 2; k++) {
+ 			X ans = y[k + 1] - y[k] - temp[n - 1] * (x[k + 1] - x[k]);
+ 			for (size_t j = res + 1; j < k; j++) {
+ 				ans -= temp[j] * (x[k + 1] - x[k]);
+ 			}
+ 			temp[k] = ans / (x[k + 1] - x[k]);
+ 		}
+ 		f = &temp[0];
+
+ 		return res;
 	}
 
 	// Expected value of payoff.
@@ -27,6 +56,15 @@ namespace fms {
 	template<class X = double>
 	inline X pwlinear_value(size_t n, const X* f, size_t i, const X* p, const X* c)
 	{
-		return 0; //!!! implement
+		//!!! implement
+ 		double value = 0;
+ 		value += f[0];
+ 		for (size_t j = 1; j <= i; j++) {
+ 			value += (f[j] * p[j]);
+ 		}
+ 		for (size_t j = i + 1; j <= n - 2; j++) {
+ 			value += (f[j] * c[j]);
+ 		}
+ 		return value; 
 	}
 }
