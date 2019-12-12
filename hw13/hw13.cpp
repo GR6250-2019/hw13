402a4f81dc94bf14d30f0522e7409827ff62653c
Li Zhuyan <18562681073@163.com>
2019-12-12 18:40:18 -0500

    hw13-zl2776

--- hw13/hw13.cpp
+++ hw13/hw13.cpp
@@ -1,6 +1,7 @@
 // xllproject.cpp
 #include <cmath>
 #include "hw13.h"
+#include "pwlinear.h"
 
 using namespace xll;
 
@@ -13,7 +14,7 @@ AddIn xai_project(
 );
 
 AddIn xai_xllproject(
-    Document(CATEGORY)
+	Document(CATEGORY)    
     .Category(CATEGORY)
     .Documentation(
         L"Documentation for the " CATEGORY L" category. "
@@ -29,3 +30,29 @@ AddIn xai_xllproject(
 );
 
 //!!! Implement XLL.PWLINEAR.VALUE as described above.
+AddIn xai_pwlinear_value(
+	Function(XLL_DOUBLE, L"?xll_pwlinear_value", L"XLL.PWLINEAR.VALUE")
+	.Arg(XLL_DOUBLE, L"px", L"is the size of x.")
+	.Arg(XLL_FP, L"py", L"is an array of y. ")
+	.Arg(XLL_DOUBLE, L"a", L"the the par forwar value.")
+	.Arg(XLL_FP, L"pp", L"is an array of puts. ")
+	.Arg(XLL_FP, L"pc", L"is an array of calls. ")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Computes piecewice linear payoff. ")
+	.Documentation(L"Implement Carr Madam formula. ")
+);
+
+ double WINAPI xll_pwlinear_value(_FP12* px, _FP12* py, double a, _FP12* pp, _FP12* pc)
+{
+#pragma XLLEXPORT
+	 auto n = size(*px);
+	 double* x = &px->array[0];
+	 double* y = &py->array[0];
+	 double* p = &pp->array[0];
+	 double* c = &pc->array[0];
+
+	 double* f = &px->array[0];
+	 size_t i = fms::pwlinear_coefficients(a, n, x, y, f);
+
+	 return fms::pwlinear_value(n, f, i, p, c);
+}
--- hw13/pwlinear.h
+++ hw13/pwlinear.h
@@ -1,6 +1,7 @@
 // pwlinear.h - Fit a piecewise linear curve.
 #pragma once
 
+
 namespace fms {
 
 	// Given a p.w. linear curve find the Carr-Madan coefficients.
@@ -15,7 +16,32 @@ namespace fms {
 	inline size_t pwlinear_coefficients(const X& a, size_t n, const X* x, const X* y, X* f)
 	{
 		//!!! implement
-		return 0;
+		size_t i;
+		for (i = 0; i < n; i++)
+			if (a > x[i] && a < x[i + 1])
+				break;
+
+		f[n - 1] = (y[i + 1] - y[i]) / (x[i + 1] - x[i]);
+		for (size_t k = i; k > 0; k--)
+		{
+			X temp = y[k - 1] - y[k] - f[n - 1] * (x[k - 1] - x[k]);
+			for (size_t j = i; j > k; j--)
+			{
+				temp -= f[j] * (x[k] - x[k - 1]);
+			}
+			f[k] = temp / (x[k] - x[k - 1]);
+		}
+		for (size_t k = i + 1; k <= n - 2; k++)
+		{
+			X temp = y[k + 1] - y[k] - f[n - 1] * (x[k + 1] - x[k]);
+			for (size_t j = i + 1; j < k; j++)
+			{
+				temp -= f[j] * (x[k + 1] - x[k]);
+			}
+			f[k] = temp / (x[k + 1] - x[k]);
+		}		
+			
+		return i;
 	}
 
 	// Expected value of payoff.
@@ -27,6 +53,12 @@ namespace fms {
 	template<class X = double>
 	inline X pwlinear_value(size_t n, const X* f, size_t i, const X* p, const X* c)
 	{
-		return 0; //!!! implement
+		//size_t coef = pwlinear_coefficients(a, n, p, c, f);
+		X result = f[0];
+		for (size_t j = 1; j <= i; j++)
+			result += f[j] * p[j];
+		for (size_t j = i + 1; j < n; j++)
+			result += f[j] * c[j];
+		return result; //!!! implement
 	}
 }
