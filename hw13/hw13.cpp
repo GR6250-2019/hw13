bf4600bfc570083ec89cfd568b4df78f46350d5b
Mingchen Li <34214579+don-ttake@users.noreply.github.com>
2019-12-12 16:59:28 -0500

    ML4403

--- hw13/hw13.cpp
+++ hw13/hw13.cpp
@@ -3,7 +3,6 @@
 #include "hw13.h"
 
 using namespace xll;
-
 AddIn xai_project(
     Document(L"hw13")
     .Documentation(
@@ -29,3 +28,34 @@ AddIn xai_xllproject(
 );
 
 //!!! Implement XLL.PWLINEAR.VALUE as described above.
+
+AddIn xai_pwlinear_value(
+	Function(XLL_DOUBLE, L"?xll_pwlinear_value", CATEGORY L".PWLINEAR.VALUE")
+	.Arg(XLL_FP, L"x", L"x values of the function")
+	.Arg(XLL_FP, L"y", L"y values of the function")
+	.Arg(XLL_DOUBLE, L"f", L"empty array for calculation")
+	.Arg(XLL_FP, L"p", L"y values of the function")
+	.Arg(XLL_FP, L"c", L"fixd a value")
+	.Uncalced()
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return i such that x[i] < a < x[i+1]")
+	.Documentation(
+		L"delcare and modifies the array f such that f is composed with "
+		L" (f(a), f''(x[1]) ... f''(x[n-2]), f'(a))"
+	)
+);
+double WINAPI xll_pwlinear_value( _FP12* xpt,  _FP12* ypt,  double f,  _FP12* ppt,  _FP12* cpt){
+#pragma XLLEXPORT
+
+	double* x = &xpt->array[0];
+	double* y = &ypt->array[0];
+	double* p = &ppt->array[0];
+	double* c = &cpt->array[0];
+
+	size_t n = size(*xpt);
+	double* coef = new double[n];
+	size_t loc = fms::pwlinear_coefficients(f, n, x, y, coef);
+	double result = fms::pwlinear_value(n, coef, loc, p, c);
+	
+	return result;
+}
--- hw13/pwlinear.h
+++ hw13/pwlinear.h
@@ -14,8 +14,48 @@ namespace fms {
 	template<class X = double>
 	inline size_t pwlinear_coefficients(const X& a, size_t n, const X* x, const X* y, X* f)
 	{
-		//!!! implement
-		return 0;
+		//start with i such that x[i] < a < x[i+1]
+
+		size_t xhat=0; 
+		int i;
+		for (i = 0; (size_t)i <n-1 ; i++) {
+			if (x[i] < a && x[i + 1]>a) {
+				xhat = i;
+			}
+			else if (x[i] == a) {
+				xhat = i;
+			}
+		}
+
+		X fppsum;
+		int j;
+		//start from xhat and go to zero to find double
+		//derivates
+		X fpa = (y[xhat] - y[xhat + 1]) / (x[xhat + 1] - x[xhat]);
+		X fa = y[xhat] - fpa * (x[xhat] - a);
+		X tempsum;
+		for (i = xhat-1; i >=0; i--) {
+			tempsum = fpa * (x[i] - a) + fa;
+			fppsum = 0;
+			for (j = i+1; (size_t)j < xhat; j++) {
+				fppsum += f[j ] * (x[j] - x[i]);
+			}
+			f[i + 1] = (y[i] - tempsum - fppsum) / (x[i + 1] - x[i]);
+		}
+
+		//start from xhat +1 and go to n to find double
+		//derivates
+		for (i = xhat +2; (size_t)i <=n-1 ; i++) {
+			tempsum = fpa * (x[i] - a) + fa;
+			fppsum = 0;
+			for (j = i-1; (size_t)j > xhat+1; j--) {
+				fppsum += f[j] * (x[i] - x[j]);
+			}
+			f[i -1] = (y[i] - tempsum - fppsum) / (x[i ] - x[i-1]);
+		}
+		f[0] = fa;
+		f[n - 1] = fpa;
+		return xhat;
 	}
 
 	// Expected value of payoff.
@@ -24,9 +64,22 @@ namespace fms {
 	//           + sum_{a < x[i]} f''(x[i]) c[i].
 	// The array f[] contains the coefficents from the function above.
 	// Assume E[X] = a so the second term is 0.
+	// Array f is filled with (f(a), f''(x[1]) ... f''(x[n-2]), f'(a))
+
 	template<class X = double>
 	inline X pwlinear_value(size_t n, const X* f, size_t i, const X* p, const X* c)
 	{
-		return 0; //!!! implement
+		X ev = f[0];
+		int j;
+		for (j = 0; (size_t)j < n-1; j++) {
+			if ((size_t)j < i) {
+				ev += f[j] * p[j];
+			}
+			else if ((size_t)j > i) {
+				ev += f[j] * c[j];
+			}
+		}
+		return ev;
+		
 	}
 }
