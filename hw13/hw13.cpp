658c598732f76291e87dbd548f995615d4c74eec
LZ2735 <55263722+LZ2735@users.noreply.github.com>
2019-12-12 19:38:06 -0500

    lz2735_hw13

--- hw13/hw13.cpp
+++ hw13/hw13.cpp
@@ -1,7 +1,7 @@
 // xllproject.cpp
 #include <cmath>
 #include "hw13.h"
-
+using namespace fms;
 using namespace xll;
 
 AddIn xai_project(
@@ -29,3 +29,26 @@ AddIn xai_xllproject(
 );
 
 //!!! Implement XLL.PWLINEAR.VALUE as described above.
+AddIn xai_pwlinear(
+	Function(XLL_DOUBLE, L"?xll_pwlinear_value", L"XLL.PWLINEAR.VALUE")
+	.Arg(XLL_FP, L"x", L"is an array of the strike price. ")
+	.Arg(XLL_FP, L"y", L"is an array of the option payoff. ")
+	.Arg(XLL_DOUBLE, L"f", L"is the par forward value of the underlying. ")
+	.Arg(XLL_FP, L"p", L"is an array of the put payoff when x < f. ")
+	.Arg(XLL_FP, L"c", L"is an array of the call payoff when x > f. ")
+	.Category(L"XLL")
+	.FunctionHelp(L"Return the value of the piecewise linear payoff. ")
+	.Documentation(L"Doc")
+);
+double WINAPI xll_pwlinear_value(_FP12* xx, _FP12* yy, double f, _FP12* pp, _FP12* cc) {
+#pragma XLLEXPORT
+	size_t n = size(*xx);
+	double* x = &xx->array[0];
+	double* y = &yy->array[0];
+	double* p = &pp->array[0];
+	double* c = &cc->array[0];
+	double* coef = &xx->array[0];
+	size_t m = pwlinear_coefficients(f, n, x, y, coef);
+	double result = pwlinear_value(n, coef, m, p, c);
+	return result;
+}
