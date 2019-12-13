9ab8b6e848f4df36fbd425aee5814167a50944f4
Fei Q <feiwqiu@gmail.com>
2019-12-12 19:23:22 -0500

    hw13

--- hw13/hw13.cpp
+++ hw13/hw13.cpp
@@ -1,8 +1,11 @@
 // xllproject.cpp
 #include <cmath>
 #include "hw13.h"
+#include "pwlinear.h"
 
 using namespace xll;
+using namespace fms;
+using namespace std;
 
 AddIn xai_project(
     Document(L"hw13")
@@ -28,4 +31,42 @@ AddIn xai_xllproject(
     )
 );
 
-//!!! Implement XLL.PWLINEAR.VALUE as described above.
+
+
+AddIn xai_pwlinear_value(
+	Function(XLL_DOUBLE, L"?xll_pwlinear_value", CATEGORY L".PWLINEAR.VALUE")
+	.Arg(XLL_FP, L"x", L"is the array of x.")
+	.Arg(XLL_FP, L"y", L"is the array of y.")
+	.Arg(XLL_DOUBLE, L"f", L"is par forward value of the underlying.")
+	.Arg(XLL_FP, L"p", L"is the array of prices of put.")
+	.Arg(XLL_FP, L"c", L"is the array of prices of call.")
+	.Category(L"XLL")
+	.FunctionHelp(L"computes the value of a piecwise linear payoff determinded by x and y")
+	.Documentation(
+		L" using put prices (x < f) and call prices (x > f). "
+		L" assume f is par forward value of the underlying. "
+	)
+);
+
+double WINAPI xll_pwlinear_value(_FP12* x, _FP12* y, double f, _FP12* p, _FP12* c)
+{
+#pragma XLLEXPORT
+
+	size_t n = size(*x);
+
+	double* x1 = &x->array[0];
+	double* y1 = &y->array[0];
+	double* p1 = &p->array[0];
+	double* c1 = &c->array[0];
+	double* coeff = &x->array[0];
+
+	size_t i = pwlinear_coefficients(f, n, x1, y1, coeff);
+    
+	try {
+        auto result = pwlinear_value(n, coeff, i, p1, c1);
+		return result;
+    }
+    catch (const std::exception& ex) {
+        XLL_ERROR(ex.what());
+		return 0;
+    }
